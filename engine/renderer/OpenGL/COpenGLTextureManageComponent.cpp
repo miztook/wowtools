5f54fe39aef20c598a1cccbdba8b6d38c3da91d9
Miaoyu <miaoyu1984@126.com>
2019-06-05 22:13:24 +0800

    add

---
+++ engine/renderer/OpenGL/COpenGLTextureManageComponent.cpp
@@ -0,0 +1,69 @@
+#include "COpenGLTextureManageComponent.h"
+
+#include "COpenGLTexture.h"
+#include "COpenGLRenderTarget.h"
+#include "CCImage.h"
+
+COpenGLTextureManageComponent::COpenGLTextureManageComponent(const COpenGLDriver* driver)
+	: Driver(driver)
+{
+
+}
+
+COpenGLTextureManageComponent::~COpenGLTextureManageComponent()
+{
+
+}
+
+ITexture* COpenGLTextureManageComponent::getManualTexture(const char* name) const
+{
+	auto itr = TextureMap.find(name);
+	if (itr == TextureMap.end())
+		return nullptr;
+
+	return itr->second;
+}
+
+ITexture* COpenGLTextureManageComponent::addTexture(const char* name, std::shared_ptr<CCImage> image, bool mipmap)
+{
+	if (TextureMap.find(name) != TextureMap.end())
+		return nullptr;
+
+	COpenGLTexture* tex = new COpenGLTexture(Driver, mipmap);
+	if (!tex->createFromImage(image))
+	{
+		delete tex;
+		return nullptr;
+	}
+	TextureMap[name] = tex;
+	return tex;
+}
+
+void COpenGLTextureManageComponent::removeTexture(const char* name)
+{
+	auto itr = TextureMap.find(name);
+	if (itr == TextureMap.end())
+		return;
+
+	ITexture* tex = itr->second;
+
+	if (tex)
+		delete tex;
+
+	TextureMap.erase(itr);
+}
+
+void COpenGLTextureManageComponent::loadDefaultTextures()
+{
+	ECOLOR_FORMAT format = ECF_R5G6B5;
+	dimension2d size(32, 32);
+
+	uint32_t pitch, nbytes;
+	getImagePitchAndBytes(format, size.width, size.height, pitch, nbytes);
+
+	uint8_t* data = new uint8_t[nbytes];
+	memset(data, 0xff, nbytes);
+
+	std::shared_ptr<CCImage> img = std::make_shared<CCImage>(format, size, data, true);
+	DefaultWhite = addTexture("$DefaultWhite", img, true);
+}
---
+++ engine/renderer/OpenGL/COpenGLTextureManageComponent.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include "base.h"
+#include <memory>
+#include <map>
+#include <list>
+
+class COpenGLDriver;
+class ITexture;
+class IRenderTarget;
+class CCImage;
+
+class COpenGLTextureManageComponent
+{
+private:
+	DISALLOW_COPY_AND_ASSIGN(COpenGLTextureManageComponent);
+
+public:
+	explicit COpenGLTextureManageComponent(const COpenGLDriver* driver);
+	~COpenGLTextureManageComponent();
+
+public:
+	ITexture* getDefaultWhite() const { return DefaultWhite; }
+
+	ITexture* getManualTexture(const char* name) const;
+	ITexture* addTexture(const char* name, std::shared_ptr<CCImage> image, bool mipmap);
+	void removeTexture(const char* name);
+
+private:
+	void loadDefaultTextures();
+
+private:
+	ITexture*	DefaultWhite;
+
+	std::map<std::string, ITexture*>	TextureMap;
+	std::list<IRenderTarget*>	RenderTargets;
+
+	const COpenGLDriver*	Driver;
+};
