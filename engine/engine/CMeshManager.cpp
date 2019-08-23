461a0b63d95e9a2c55b8102e7537b84e92c372ca
Miaoyu <miaoyu1984@126.com>
2019-08-23 23:52:59 +0800

    update

--- engine/engine/CMeshManager.cpp
+++ engine/engine/CMeshManager.cpp
@@ -7,10 +7,74 @@ CMeshManager::CMeshManager()
 
 CMeshManager::~CMeshManager()
 {
-	for (auto itr = MeshMap.begin(); itr != MeshMap.end(); ++itr)
+	for (auto & itr : MeshMap)
 	{
-		delete itr->second;
+		delete itr.second;
 	}
 
 	MeshMap.clear();
+}
+
+void CMeshManager::getCubeMeshVICount(uint32_t& vcount, uint32_t& icount)
+{
+	vcount = 12;
+	icount = 36;
+}
+
+bool CMeshManager::fillCubeMeshVI(SVertex_PCT* vertices, uint32_t vcount, uint16_t* indices, uint32_t icount, const vector3df& size, SColor color /*= SColor::White()*/)
+{
+	uint32_t vLimit, iLimit;
+	getCubeMeshVICount(vLimit, iLimit);
+
+	if (vcount < vLimit || icount < iLimit)
+		return false;
+
+	const uint16_t u[36] = { 0, 2, 1, 0, 3, 2, 1, 5, 4, 1, 2, 5, 4, 6, 7, 4, 5, 6,
+		7, 3, 0, 7, 6, 3, 9, 5, 2, 9, 8, 5, 0, 11, 10, 0, 10, 7 };
+
+	Q_memcpy(indices, sizeof(uint16_t) * 36, u, sizeof(u));
+
+	vertices[0].set(vector3df(0, 0, 0), color, vector2df(0, 1));
+	vertices[1].set(vector3df(1, 0, 0), color, vector2df(1, 1));
+	vertices[2].set(vector3df(1, 1, 0), color, vector2df(1, 0));
+	vertices[3].set(vector3df(0, 1, 0), color, vector2df(0, 0));
+	vertices[4].set(vector3df(1, 0, 1), color, vector2df(0, 1));
+	vertices[5].set(vector3df(1, 1, 1), color, vector2df(0, 0));
+	vertices[6].set(vector3df(0, 1, 1), color, vector2df(1, 0));
+	vertices[7].set(vector3df(0, 0, 1), color, vector2df(1, 1));
+	vertices[8].set(vector3df(0, 1, 1), color, vector2df(0, 1));
+	vertices[9].set(vector3df(0, 1, 0), color, vector2df(1, 1));
+	vertices[10].set(vector3df(1, 0, 1), color, vector2df(1, 0));
+	vertices[11].set(vector3df(1, 0, 0), color, vector2df(0, 0));
+
+	for (uint32_t i = 0; i < 12; ++i)
+	{
+		vertices[i].Pos -= vector3df(0.5f, 0.5f, 0.5f);
+		vertices[i].Pos = vertices[i].Pos * size;
+	}
+
+	return true;
+}
+
+void CMeshManager::getSphereMeshVICount(uint32_t& vcount, uint32_t& icount, uint32_t polyCountX, uint32_t polyCountY)
+{
+	if (polyCountX < 2)
+		polyCountX = 2;
+	if (polyCountY < 2)
+		polyCountY = 2;
+	while (polyCountX * polyCountY > 32767) // prevent uint16_t overflow
+	{
+		polyCountX /= 2;
+		polyCountY /= 2;
+	}
+
+	const uint32_t polyCountXPitch = polyCountX + 1; // get to same vertex on next level
+
+	vcount = polyCountXPitch * polyCountY + 2;
+	icount = (polyCountX * polyCountY) * 6;
+}
+
+bool CMeshManager::fillSphereMeshVI(SVertex_PCT* vertices, uint32_t vcount, uint16_t* indices, uint32_t icount, float radius /*= 5.f*/, uint32_t polyCountX /*= 16*/, uint32_t polyCountY /*= 16*/, SColor color /*= SColor::White()*/)
+{
+
 }
--- engine/engine/CMeshManager.h
+++ engine/engine/CMeshManager.h
@@ -2,6 +2,8 @@
 
 #include "base.h"
 #include <map>
+#include "aabbox3d.h"
+#include "S3DVertex.h"
 
 class CMesh;
 
@@ -15,7 +17,35 @@ public:
 	~CMeshManager();
 
 public:
+	static void getCubeMeshVICount(uint32_t& vcount, uint32_t& icount);
+	static bool fillCubeMeshVI(SVertex_PCT* vertices, uint32_t vcount, uint16_t* indices, uint32_t icount,
+		const vector3df& size, SColor color = SColor::White());
+	static aabbox3df getCubeMeshAABBox(const vector3df& size) { return aabbox3df(-size / 2, size / 2); }
 
+	static void getSphereMeshVICount(uint32_t& vcount, uint32_t& icount, uint32_t polyCountX, uint32_t polyCountY);
+	static bool fillSphereMeshVI(SVertex_PCT* vertices, uint32_t vcount, uint16_t* indices, uint32_t icount,
+		float radius = 5.f,
+		uint32_t polyCountX = 16, uint32_t polyCountY = 16, SColor color = SColor::White());
+	static aabbox3df getSphereMeshAABBox(float radius)
+	{
+		return aabbox3df(vector3df(-radius, -radius, -radius), vector3df(radius, radius, radius));
+	}
+
+	static void getCylinderMeshVICount(uint32_t& vcount, uint32_t& icount, uint32_t numSegment);
+	static bool fillCylinderMeshVI(SVertex_PCT* vertices, uint32_t vcount, uint16_t* indices, uint32_t icount,
+		float radius, float height, uint32_t numSegment = 20, SColor color = SColor());
+	static aabbox3df getCylinderMeshAABBox(float radius, float height)
+	{
+		return aabbox3df(vector3df(-radius, -height, -radius), vector3df(radius, height, radius));
+	}
+
+	static uint32_t getGridLineVCount(uint32_t xzCount) { return (2 * xzCount + 1) * 4; }
+	static bool fillGridLineMeshV(SVertex_PC* gVertices, uint32_t vcount, uint32_t xzCount, float gridSize, SColor color);
+	static aabbox3df getGridLineAABBox(uint32_t xzCount, float gridSize)
+	{
+		float halfWidth = xzCount * gridSize;
+		return aabbox3df(vector3df(-halfWidth, 0, -halfWidth), vector3df(halfWidth, 0, halfWidth));
+	}
 
 private:
 	std::map<std::string, CMesh*>	MeshMap;
