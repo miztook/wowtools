0a595cbfaadd69d21ac191773c8d339e00bb6953
Miaoyu <miaoyu1984@126.com>
2019-12-11 22:35:41 +0800

    update

--- tools/TestLexer/TestLexer.cpp
+++ tools/TestLexer/TestLexer.cpp
@@ -4,6 +4,16 @@
 #include <iostream>
 #include <list>
 
+#include "CFileSystem.h"
+#include "wowEnvironment.h"
+#include "wowDatabase.h"
+#include "function.h"
+#include "stringext.h"
+
+#include "ScriptLexer.h"
+
+void testLexer();
+
 int main(int argc, char* argv[])
 {
 #if defined(DEBUG) | defined(_DEBUG)
@@ -12,4 +22,31 @@ int main(int argc, char* argv[])
 
 	getchar();
 	return 0;
+}
+
+void testLexer()
+{
+	char workingDirectory[QMAX_PATH];
+	Q_getcwd(workingDirectory, QMAX_PATH);
+
+	CFileSystem* fs = new CFileSystem(workingDirectory, R"(D:\World Of Warcraft 81)");
+
+	//
+	std::string dir = fs->getWorkingDirectory();
+	normalizeDirName(dir);
+
+	dir += "Shaders/";
+	Q_MakeDirForFileName(dir.c_str());
+
+	{
+		CReadFile* rf = fs->createAndOpenFile((dir + "Redify.shader").c_str(), false);
+		char* content = new char[rf->getSize()];
+		rf->readText(content, rf->getSize());
+
+		delete[] content;
+		delete rf;
+	}
+
+
+	delete fs;
 }
